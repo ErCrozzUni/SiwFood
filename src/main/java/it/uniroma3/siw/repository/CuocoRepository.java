515a05248947f8bc75b235f2302c6c747b27e5e3
ErCrozzUni <leo.crozzoli@stud.uniroma3.it>
2024-05-29 12:50:36 +0200

    COMMIT SGRAVATOOOOOOOOOOO

--- src/main/java/it/uniroma3/siw/repository/CuocoRepository.java
+++ src/main/java/it/uniroma3/siw/repository/CuocoRepository.java
@@ -2,6 +2,7 @@ package it.uniroma3.siw.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import it.uniroma3.siw.model.Cuoco;
@@ -9,4 +10,6 @@ import it.uniroma3.siw.model.Cuoco;
 public interface CuocoRepository extends CrudRepository<Cuoco, Long> {
 	Cuoco findByCredenzialiUsername(String username);
 	Optional<Cuoco> findById(Long id);
+	@Query("SELECT c FROM Cuoco c LEFT JOIN FETCH c.ricette WHERE c.id = :id")
+	Cuoco findByIdWithRicette(Long id);
 }
